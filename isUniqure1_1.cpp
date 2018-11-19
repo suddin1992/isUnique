985f83fb555beedaa383d5739b55cc003b735a61
sharif uddin <39477291+suddin1992@users.noreply.github.com>
2018-11-19 10:48:29 -0500

    Add files via upload

--- isUniqure1_1.cpp
+++ isUniqure1_1.cpp
@@ -3,16 +3,17 @@
 using namespace std;
 
 bool isUnique(string nString) //boolen function returns 0 or 1 based on the codes below
-{
-	for (int i=0; i<nString.length();i++){ //first for loop to compare
-		for (int j=0; j<nString.length();j++){ //Second for loop to compare with the first
-			if (nString[i] != nString[j]){ //this line checks is any characters are repeating
-				return true; //if any character repeats then return true (1)
+{	
+	int sLength=nString.length();
+	for (int i=0; i<sLength;i++){ //first for loop to compare
+		for (int j=i+1; j<sLength;j++){ //Second for loop to compare with the first
+			if (nString[i] == nString[j]){ //this line checks is any characters are repeating
+				return false; //if any character repeats then return true (1)
 			}
 		}
 	}
 	
-	
+	return true;
 }
 
 int main()
